c793162e9dca79f2d5846b2b223eab7066148880
Joanna Wasiluk <jmwasiluk@yahoo.com>
2017-05-24 14:46:32 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
-  
   # GET to /users/:user_id/profile/new  
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end  
 end
