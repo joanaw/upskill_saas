eb7f10007ba628fc6ea6cb4f6b42616355e146b8
Joanna Wasiluk <jmwasiluk@yahoo.com>
2017-05-30 16:21:40 +0000

    Added before action filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
+  # Action to authenticate users defined by Devise gem.
+  before_action :authenticate_user!
+  # Action described below.
+  before_action :only_current_user
+  
   # GET request to /users/:user_id/profile/new.  
   def new
     # Render blank profile details form.
@@ -19,20 +24,20 @@ class ProfilesController < ApplicationController
     end
   end
   
-  # GET request to /users/:user_id/profile/edit
+  # GET request to /users/:user_id/profile/edit .
   def edit
     # Retrieve user from the database
     @user = User.find( params[:user_id])
     @profile = @user.profile
   end
   
-  # PUT request to /users/:user_id/profile
+  # PUT request to /users/:user_id/profile .
   def update
     # Retrieve user from the database
     @user = User.find( params[:user_id] )
     # Retrive that user's profile
     @profile = @user.profile
-    # Mass assign edited profile attribiutes and save (update)
+    # Mass assign edited profile attribiutes and save (update).
     if @profile.update_attributes(profile_params) 
       flash[:success] = "Pofile updated!"
       # Redirect user to their profile page
@@ -42,8 +47,14 @@ class ProfilesController < ApplicationController
     end
   end
   
+  # This section called "private" is run only inside this file.
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
-  end  
+  end
+  
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # Action to authenticate users defined by Devise gem.
+  before_action :authenticate_user!
+  
   # GET request to /users/:id
   def show
   @user = User.find( params[:id] )
