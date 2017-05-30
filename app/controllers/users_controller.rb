c91b5034c46906799d3bad93d4a71298592b51e3
Joanna Wasiluk <jmwasiluk@yahoo.com>
2017-05-30 17:10:57 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   # Action to authenticate users defined by Devise gem.
   before_action :authenticate_user!
   
+  def index
+    
+  end
+  
   # GET request to /users/:id
   def show
   @user = User.find( params[:id] )
