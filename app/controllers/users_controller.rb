107ca5a2408806bbea3f8c98cff2d0f416f5e755
Joanna Wasiluk <jmwasiluk@yahoo.com>
2017-05-30 18:48:05 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    
+    @users = User.includes(:profile)
   end
   
   # GET request to /users/:id
